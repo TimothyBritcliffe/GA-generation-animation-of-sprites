5ac75ba0083aeb29b69d94ba1a6c23b95f66eba6
Timothy Britcliffe <Timothy@britcliffe.com>
2026-04-09 16:49:49 -0700

    Modified the loops to generate 20 new individuals from the 10 best selected, and add on the remaining 10 randomly selected individuals to create a new population

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import time
 from typing import List, Tuple
+import os
+os.add_dll_directory(r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.1\bin")
 import numpy as np
 from genetic_algorithm import initialize_population, calculate_fitness, selection, crossover, mutate, decode
 
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     side = 8
     size = side ** 2
 
@@ -15,32 +19,39 @@ if __name__ == '__main__':
     IMG_2_arr = np.round(np.random.uniform(0.1, 0.9, size), 1)
     A_arr = np.random.randint(-5, 5, (size, size))
 
-    generation: List[List[int]] = initialize_population()
+    population: List[List[int]] = initialize_population()
     generations: int = 30
 
     for i in range(0, generations):
         scored_pop: List[Tuple[List[int], float]] = []
 
-        for individual in generation:
+        for individual in population:
             score = calculate_fitness(individual, IMG_1_arr, IMG_2_arr, A_arr)
             scored_pop.append((individual, score))
 
-        best_ind: List[int] = selection(scored_pop)
+        new_population: List[List[int]] = []
+        parents, random_list = selection(scored_pop)
+        while len(new_population) < 20:
+            for j in range(0, len(parents), 2): #Will throw an Index out of Bounds error if the number of parents selected is not even currently
+                parent1 = parents[j]
+                parent2 = parents[j+1]
 
-        new_generation: List[List[int]] = [best_ind]
+                child1 = crossover(parent1, parent2)
+                child2 = crossover(parent1, parent2)
 
-        while len(new_generation) < 10:
-            p1 = selection(scored_pop)
-            p2 = selection(scored_pop)
+                mutate(child1, 0.01) #0.01 = 1% mutation rate
+                mutate(child2, 0.01)
 
-            child = crossover(p1, p2)
-            child = mutate(child, 0.01)  #1% chance of mutation
+                new_population.append(child1)
+                new_population.append(child2)
 
-            new_generation.append(child)
+        new_population.extend(random_list)
+        population = new_population
 
-        generation = new_generation
-
-    final_winner = selection(scored_pop)
+    best_individuals, _ = selection(scored_pop)
+    final_winner = best_individuals[0]
     final_r, final_inc = decode(final_winner)
     print(f"Complete. Best Radius: {final_r}, Best Increment: {final_inc}")
+    end_time = time.time()
+    print(f"Time Taken: {(end_time - start_time)/60.0} minutes")
     #generate_animation_gif() (not implemented)
