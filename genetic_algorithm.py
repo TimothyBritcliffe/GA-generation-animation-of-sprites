416c9234ea2bbfcb6bb79185ad4b169f727c42d2
Timothy Britcliffe <Timothy@britcliffe.com>
2026-04-09 16:50:55 -0700

    Modified the selection algorithm to return the 10 best individuals along with a list of 10 randomly selected individuals.

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,17 +1,21 @@
 import random
 from typing import List, Tuple
+import os
+os.add_dll_directory(r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.1\bin")
 import numpy as np
 from math_calculations import run_iterations
 
 
+
 #Initializes by default 10 individuals (32 bit strings) - First 16 bits are the radius, second half are the increment
-def initialize_population(size:int = 10) -> List[List[int]]:
+def initialize_population(size:int = 30) -> List[List[int]]:
     individuals_=[]
 
     for _ in range(size):
         individuals_.append([random.randint(0, 1) for _ in range(32)])
 
     return individuals_
+
 
 #Takes the individual and breaks it down into radius and increment (and converts into decimal from binary)
 def decode(ind:List[int]) -> Tuple[float,float]:
@@ -24,8 +28,8 @@ def decode(ind:List[int]) -> Tuple[float,float]:
     radius: int = int(radius, 2)
     increment: int = int(increment, 2)
 
-    radius /= 1000000
-    increment /= 100000
+    radius /= 100000
+    increment /= 1000000
 
     return radius, increment
 
@@ -35,29 +39,38 @@ def calculate_fitness(individual:List[int], img1:np.ndarray, img2:np.ndarray, A:
     radius, increment = decode(individual)
     x, lam, r, inc, r_initial, inc_initial = run_iterations(30, img1, img2, A, 0.001, radius, increment)
 
-    if lam > 1.1:
+    if lam > 1.05:
         score: float = 0.0
     else:
-        score: float = 1.0 - abs(1-lam)
+        #score: float = 1.0 - abs(1-lam)
+        score: float = lam
 
     return score
 
 
-#Checks the population and returns the winning individual that gets to be a parent
-#Winning parent selected by Tournament - if elitism preferred we can just remove the sample so it finds the best overall
-def selection(pop_with_scores:List[Tuple[List[int], float]]) -> List[int]:
-    sample = random.sample(pop_with_scores, 3)
-    best_ind: List[int] = sample[0][0]
-    highest_score: float = sample[0][1]
+#Sorts the overall population by score (in descending order) takes the best 10 from that list and gets a random set of individuals
+#Random individual selection count is determined by the target size (if the # of individuals after selecting the top 10 is under 10, then it just selects the remaining, otherwise by default it is 10)
+def selection(pop_with_scores:List[Tuple[List[int], float]]) -> Tuple[List[List[int]], List[List[int]]]:
+    pop_with_scores.sort(key=lambda x: x[1], reverse=True)
 
-    for ind in sample:
-        selected_ind: List[int] = ind[0]
-        current_score: float = ind[1]
-        if current_score > highest_score:
-            highest_score = current_score
-            best_ind = selected_ind
+    set_of_individuals = [individual[0] for individual in pop_with_scores]
 
-    return best_ind
+    best_10 = set_of_individuals[:10]
+    candidates = set_of_individuals[10:]
+
+    random_10 = []
+    target_size = min(10, len(candidates))
+
+    if target_size == 0:
+        print("Warning: Population too small to pick random survivors.")
+        return best_10, []
+
+    while len(random_10) < target_size:
+        random_ind = random.choice(candidates)
+        if random_ind not in random_10:
+            random_10.append(random_ind)
+
+    return best_10, random_10
 
 
 #Creates a new (child) individual from slicing each parent at a random point and reassembling
